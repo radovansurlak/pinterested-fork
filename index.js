1877b5d173c5bb7adea63374303d44affb364d9f
radovansurlak <radovansurlak@gmail.com>
2019-06-19 17:38:38 +0200

    added redirect to secure URL

--- index.js
+++ index.js
@@ -21,7 +21,13 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect(`https://${req.headers.host}${req.url}`);
+  }
 });
 
 app.use(express.static('dist'));
