310daa47a44e15ec7f60f09d615d3cfbb11f25df
radovansurlak <radovansurlak@gmail.com>
2019-06-20 17:33:59 +0200

    selectively importing vuetify components

--- client/index.js
+++ client/index.js
@@ -1,12 +1,27 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify';
 import SocialSharing from 'vue-social-sharing';
+
+import Vuetify, {
+  VApp, // required
+  VBtn,
+  VSelect,
+  VForm,
+  VTextField,
+} from 'vuetify/lib';
 
 import App from './app/App.vue';
 
 import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(SocialSharing);
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VBtn,
+    VSelect,
+    VForm,
+    VTextField,
+  },
+});
 
 new Vue(App).$mount('#app');
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify-es').default;
 
-gulp.task('uglifyNodeJS', () => {
+gulp.task('uglifyNodeJS', (done) => {
   gulp.src(['./index.js', './server/*.js'], { base: './' })
     .pipe(uglify())
     .pipe(gulp.dest('.'));
+  done();
 });
