b1f87bd4bc04bca5bd684fb724f950062abb1119
radovansurlak <radovansurlak@gmail.com>
2019-06-21 16:20:02 +0200

    removed serving of static files by express

--- index.js
+++ index.js
@@ -22,9 +22,6 @@ app.use(sslRedirect());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Serving static files - would be best delegated to Nginx
-app.use(express.static('dist'));
-
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
